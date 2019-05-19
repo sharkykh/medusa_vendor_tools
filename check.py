594a948ef289030591e014a31578a23328904e9f
sharkykh <sharkykh@gmail.com>
2019-05-19 09:30:06 +0300

    update check.py

--- check.py
+++ check.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-#!/usr/bin/env python
 # coding: utf-8
 """
 Check vendor folders using `ext/readme.md`.
@@ -14,8 +13,8 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import os
 import sys
+from pathlib import Path
 
 from parse_md import (
     LineParseError,
@@ -26,13 +25,9 @@ DEFAULT_INFILE = 'ext/readme.md'
 
 
 def main(infile):
-    root = os.path.abspath(
-        os.path.join(
-            os.path.dirname(infile),
-            '..',
-        )
-    )
+    root = Path(infile).parent.parent.absolute()
 
+    all_found = True
     iterator = parse_requirements(infile)
     while True:
         try:
@@ -48,13 +43,21 @@ def main(infile):
                 raise error
 
         for module in req['modules']:
+            # backports/package.py [OR] backports.module
             split_count = (module.count('.') - 1) if module.endswith('.py') else -1
             parts = module.split('.', split_count)
-            module_paths = [os.path.join(root, f, *parts) for f in req['folder']]
-            if not all(os.path.exists(p) for p in module_paths):
-                print('❌ ', module, '⚠  NOT FOUND IN:', module_paths)
-            # else:
-            #     print('✅ ', module, module_paths)
+
+            module_paths = [root.joinpath(f, *parts) for f in req['folder']]
+            rel_module_paths = [str(p.relative_to(root).as_posix()) for p in module_paths]
+
+            if not all(p.exists() for p in module_paths):
+                print('XX', module, '!!  NOT FOUND IN:', rel_module_paths)
+                all_found = False
+            else:
+                pass  # print('VV', module, rel_module_paths)
+
+    if all_found:
+        print('Done.')
 
 
 if __name__ == '__main__':
