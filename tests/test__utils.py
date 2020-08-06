907e75487d7bcfa8219186571b245a6ad6241129
sharkykh <sharkykh@gmail.com>
2020-08-07 00:01:22 +0300

    simple test for `get_renovate_config`

--- tests/test__utils.py
+++ tests/test__utils.py
@@ -1,3 +1,11 @@
+from pathlib import Path
+
 import pytest  # noqa: F401
 
-from mvt import _utils  # noqa: F401
+from mvt import _utils
+
+
+def test_():
+    fixtures_path = Path(__file__).parent.joinpath('fixtures')
+    # TODO: Actual data comparison
+    assert _utils.get_renovate_config(fixtures_path)
